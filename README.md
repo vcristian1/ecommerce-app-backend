f777dd3a50bff7834cf9267e04833accc9352761
Cristian Vargas <cristianvargas@Cristians-MacBook-Air.local>
2022-07-09 17:53:04 -0500

    added two routes in the category-routes.js file which gets all categories and category by ID

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# ecommerce-app-backend
+# Ecommerce-App-Backend
+
+## User Story
+
+AS A manager at an internet retail company
+I WANT a back end for my e-commerce website that uses the latest technologies
+SO THAT my company can compete with other e-commerce companies
+
+## Screenshots
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,26 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryID = await Category.findByPk({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryID);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
