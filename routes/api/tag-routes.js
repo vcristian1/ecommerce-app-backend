028825087644b6422f19d4c795cbd42aacd66c09
Cristian Vargas <cristianvargas@Cristians-MacBook-Air.local>
2022-07-10 17:51:22 -0500

    added post and put routes to tag-routes.js which create a new tag, and update a current tag by its ID

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -43,16 +43,52 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (error) {
+      res.status(500).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      { name: req.body.name },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    if(!tagData[0]) {
+      res.status(404).json('404 Error! No Tag found with this id!')
+    } 
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: '404 Error! No Tag can be found with this id!'});
+      return;
+    }
+    res.status(200).json("The Tag has been successfully deleted!")
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
